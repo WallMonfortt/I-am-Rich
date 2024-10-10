dd44eac357f41081478b9eec925bfae7c656b9d4
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-10-10 09:04:23 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Jose Gualberto Monfortte Flores on 10/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
